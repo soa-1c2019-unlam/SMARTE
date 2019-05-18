2153079e19ad36336606cf671defd128fe20d74d
MatiiTomaino <matiastomaino@live.com.ar>
2019-05-18 02:32:37 -0300

    Implementación para prender servos y bomba desde android

--- Android/app/src/main/java/com/example/smarteapp2/MainActivity.java
+++ Android/app/src/main/java/com/example/smarteapp2/MainActivity.java
@@ -16,9 +16,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button botonPrenderLed, botonApagarLed;
-    TextView txtEstadoLed, txtTemperatura;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,67 +47,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
-
-
-        botonPrenderLed = findViewById(R.id.prenderLedButton);
-        botonApagarLed = findViewById(R.id.apagarLedButton);
-
-        txtEstadoLed = findViewById(R.id.textLedState);
-        txtTemperatura = findViewById(R.id.textTemp);
-
-        botonPrenderLed.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Solicitar("/4/on");
-                }
-        });
-
-        botonApagarLed.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Solicitar("/4/off");
-            }
-        });
-    }
-
-    private void Solicitar(String comando){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-            String url = "http://192.168.1.200";
-
-            new SolicitarDatos().execute(url + comando);
-        }
-        else {
-            Toast.makeText(MainActivity.this, "Ninguna conexión detectada.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private class SolicitarDatos extends AsyncTask<String, Void, String>{
-
-        @Override
-        protected String doInBackground(String... url) {
-            return Conexion.getDatos(url[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String resultado){
-            if(resultado != null){
-                if(resultado.contains("GPIO 4 - State on")){
-                    txtEstadoLed.setTextColor(Color.GREEN);
-                    txtEstadoLed.setText("Led prendido");
-                    txtTemperatura.setText(resultado.split("temperatura es")[1].substring(10,15));
-                }
-                else if (resultado.contains("GPIO 4 - State off")){
-                    txtEstadoLed.setText("Led apagado");
-                    txtEstadoLed.setTextColor(Color.RED);
-                    txtTemperatura.setText(resultado.split("temperatura es")[1].substring(10,15));
-                }
-            }
-            else{
-                Toast.makeText(MainActivity.this, "Ocurrió un error.", Toast.LENGTH_LONG).show();
-            }
-        }
     }
 }
--- Android/app/src/main/java/com/example/smarteapp2/MateAlGustoActivity.java
+++ Android/app/src/main/java/com/example/smarteapp2/MateAlGustoActivity.java
@@ -2,12 +2,61 @@ package com.example.smarteapp2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MateAlGustoActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDataBase;
+    DatabaseReference databaseReference;
+
+    Button botonAzucar, botonYerba, botonAgua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mate_al_gusto);
+
+        inicializarFirebase();
+
+        botonAgua = findViewById(R.id.colocarAguaButton);
+        botonAzucar = findViewById(R.id.azucarButton);
+        botonYerba = findViewById(R.id.yerbaButton);
+
+        botonAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    databaseReference.child("bomba").setValue(1);
+                }
+            });
+
+        botonYerba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("servoYerba").setValue(1);
+            }
+        });
+
+        botonAzucar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("servoAzucar").setValue(1);
+            }
+        });
+
+
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDataBase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDataBase.getReference();
     }
 }
