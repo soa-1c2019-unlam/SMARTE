70243c2c2c55bf8b0511cf35893624d37ad4108a
MatiiTomaino <matiastomaino@live.com.ar>
2019-05-19 03:19:47 -0300

    Cambio de icono,  widget de hora para programar horario(No funca)

--- Android/app/src/main/java/com/example/smarteapp2/ProgramarHorarios.java
+++ Android/app/src/main/java/com/example/smarteapp2/ProgramarHorarios.java
@@ -1,12 +1,27 @@
 package com.example.smarteapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgramarHorarios extends AppCompatActivity {
 
@@ -16,17 +31,27 @@ public class ProgramarHorarios extends AppCompatActivity {
     Spinner spinnerPerfiles ;
     Button guardarButton;
 
+    FirebaseDatabase firebaseDataBase;
+    DatabaseReference databaseReference;
+
+    private List<Perfil> perfilList = new ArrayList<Perfil>();
+    ArrayAdapter<Perfil> arrayAdapterPerfil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programar_horarios);
 
         switchActivado = findViewById(R.id.activarSwitch);
-        textoHorario = findViewById(R.id.editText);
+        textoHorario = findViewById(R.id.editTextHorario);
         spinnerPerfiles = findViewById(R.id.spinner);
         guardarButton = findViewById(R.id.guardarButton);
 
         spinnerPerfiles.setEnabled(false);
+
+        inicializarFirebase();
+
+        ListarDatos();
 
         switchActivado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -41,5 +66,45 @@ public class ProgramarHorarios extends AppCompatActivity {
                 }
             }
         });
+
+        guardarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("programaActivado").setValue(1);
+                databaseReference.child("horarioAutomatico").setValue(textoHorario);
+            }
+        });
+    }
+
+    private void ListarDatos() {
+        databaseReference.child("Perfiles").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                perfilList.clear();
+                for (DataSnapshot objSnapShot : dataSnapshot.getChildren()){
+                    Perfil p = objSnapShot.getValue(Perfil.class);
+                    if (p == null){
+                        Toast.makeText(ProgramarHorarios.this,"Atención: No hay perfiles cargados!", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(guardarButton.getContext(), IniciarCebador.class);
+                        startActivityForResult(intent, 0);
+                    }
+                    else{
+                        perfilList.add(p);
+                        arrayAdapterPerfil = new ArrayAdapter(ProgramarHorarios.this, android.R.layout.simple_spinner_dropdown_item,perfilList);
+                        spinnerPerfiles.setAdapter(arrayAdapterPerfil);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDataBase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDataBase.getReference();
     }
 }
